88f71c2f850b3f1f0424ffc240d20b188720adfb
Randy Lin <linyejoe2@gmail.com>
2022-11-25 11:44:15 +0800

    change eslint setting

--- src/page/Survey/SurveyStep.tsx
+++ src/page/Survey/SurveyStep.tsx
@@ -12,8 +12,8 @@ export const SurveyStep: FC<PostsProps> = ({ stepId, positionData }) => {
   const { t } = useTranslation()
   return <>
     {positionData.enable
-      ? <FormControl className={ "survey-step-form" }>
-        <FormLabel className={ "survey-step-label" } id='gender'>{t(`p${stepId}.q1`)}</FormLabel>
+      ? <FormControl className={ 'survey-step-form' }>
+        <FormLabel className={ 'survey-step-label' } id='gender'>{t(`p${stepId}.q1`)}</FormLabel>
         {/* <FormLabel sx={{ fontSize: '20px' }} >{t({`p${stepId}.q2')}</FormLabel> */}
         <RadioGroup
           sx={{ mt: 2 }}
@@ -32,8 +32,8 @@ export const SurveyStep: FC<PostsProps> = ({ stepId, positionData }) => {
           <FormControlLabel value={t(`p${stepId}.a4`)} control={<Radio />} label={t(`p${stepId}.a4`)} />
         </RadioGroup>
       </FormControl>
-      : <FormControl className={ "survey-step-form" }>
-        <FormLabel className={ "survey-step-label" } id='gender'>{t(`p${stepId}.s1`)}</FormLabel>
+      : <FormControl className={ 'survey-step-form' }>
+        <FormLabel className={ 'survey-step-label' } id='gender'>{t(`p${stepId}.s1`)}</FormLabel>
         <FormLabel sx={{ fontSize: '20px' }} >{t(`p${stepId}.s2`)}</FormLabel>
       </FormControl>
     }
